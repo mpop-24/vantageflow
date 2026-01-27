360c619c8cf224bb8e06de33051440fca768f802
mpop-24 <kellpopov24@gmail.com>
2026-01-26 20:24:42 -0500

    changed slack alerts

--- slack_alerts.py
+++ slack_alerts.py
@@ -18,9 +18,9 @@ def send_price_alert(
     product_url=None,
 ):
     if new_p is None:
-        text = f"Price Change! {comp_name} price updated."
+        text = f"PRICE CHANGE | {comp_name} price updated."
     elif client_p is None:
-        text = f"Price Change! {comp_name} is now {_format_price(new_p)}."
+        text = f"PRICE CHANGE | {comp_name} is now {_format_price(new_p)}."
     else:
         diff = client_p - new_p
         if diff > 0:
